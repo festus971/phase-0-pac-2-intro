39955a84062f0621622d6afce1567d0fc50e1f3c
+ <+>
2022-05-31 13:52:02 +0300

    commit tree 6356a8dc21bbc0e092fb71adc9386cfbcfaddb4c

---
+++ index.js
@@ -0,0 +1,74 @@
+/*let favouriteMumber =32;
+if(favouriteMumber >=10){
+favouriteMumber=favouriteMumber +10}
+console.log(favouriteMumber)
+let favouriteNumber=0;
+while(favouriteNumber < 10){
+    favouriteNumber=favouriteNumber +1;
+}
+console.log(favouriteNumber)*/
+//const sum=1;
+//console.log (sum)
+const lineOne="joy to the world";
+const lineTwo="all the boys and the girls";
+const lineThree="joy to the fishes in the deep blue sea";
+const lineFour="joy to you and me";
+const chorus=`${lineOne}\n${lineTwo}\n${lineThree}\n${lineFour}`;
+console.log(chorus)
+/*const age=14;
+let isAdult;
+if(age>=18){
+    isAdult=true;
+}
+else{
+    isAdult=false;
+}
+console.log(isAdult)*/
+/*const age=26;
+const isAdult=age>=18? true:false;
+console.log(isAdult)
+
+const age1=20;
+const age1Message=age1<=18?"congratutulations! you're an adult":"enjoy your childhood while it lasts!";
+console.log(age1Message)*/
+/*const name="alice"
+let greeting;
+if(name==="alice"){
+    greeting="hello, alice!"
+}
+else if(name==="the white rabbit"){
+    greeting="dont be late, white rabbit"
+}
+else if(name==="the mad hatter"){
+    greeting="welcome to the tea party, mad hatter"
+
+}
+else{
+    greeting="whoo are you"
+}
+console.log(greeting)*/
+/*const age = 20;
+let isAdult = false,
+  canWork = false,
+  canEnlist = false,
+  canDrink = false;
+if (age >= 21) {
+  isAdult = true;
+  canWork = true;
+  canDrink = false;
+  canEnlist = true;
+} else if (age >= 18) {
+  isAdult = true;
+  canEnlist = true;
+  canWork = true;
+} else if (age >= 16) {
+  canWork = true;
+}
+console.log(canDrink,canEnlist,canWork,isAdult);
+console.log(
+    `Age: ${age},\nCan work ${canWork},\nCan enlist: ${canEnlist}, Is a legal adult: ${isAdult}, Can drink: ${canDrink}`
+  );
+  */
+ while(null){
+     console.log("i will never run..")
+ }
